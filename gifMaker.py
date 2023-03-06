83a533f99fc750c9ecd41c6bffb47d79c76daa51
genus13 <118463395+genus13@users.noreply.github.com>
2023-03-06 10:35:23 +0100

    Add files via upload

---
+++ gifMaker.py
@@ -0,0 +1,20 @@
+
+'''
+    After SHOW_TURBSIM_DATA has been runned this script produce
+    a .gif animation
+'''
+
+import imageio
+import os
+from natsort import natsorted
+
+in_path = 'temp/'
+out_filename="animation"
+out_path="results/"
+in_filenames = natsorted(os.listdir(in_path))
+
+with imageio.get_writer(out_path+out_filename+".gif", mode='I', duration='0.25') as writer:
+    for in_filename in in_filenames:
+        print(in_path+in_filename)
+        image = imageio.imread(in_path + in_filename)
+        writer.append_data(image)
